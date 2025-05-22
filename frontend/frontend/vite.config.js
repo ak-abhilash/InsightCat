ab7149227c8c99658010102694d5ec39147d5a35
Abhilash Krishna <abhilashkrishna444@gmail.com>
2025-05-22 23:07:18 +0530

    Updated

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),  // This maps '@' to your src folder
+    },
+  },
   build: {
-    outDir: 'build'  
+    outDir: 'build'
   }
 })
